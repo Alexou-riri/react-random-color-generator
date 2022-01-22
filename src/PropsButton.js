1c232c1a83743d01b67f30cfabd488668700c703
Alex <alexandra.rieux@gmx.com>
2022-01-22 14:28:34 +0100

    Final version sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- src/PropsButton.js
+++ src/PropsButton.js
@@ -3,41 +3,44 @@ import React, { useState } from 'react';
 import './App.css';
 
 // function for random color
-export default function RandomColor(){
-   const[color, setColor] = useState('#000'); // update color
-   const getRgb = () => Math.floor(Math.random() * 256); // generate random numbers
-   const rgbToHex = (r, g, b) =>
-    '#' + [r, g, b]
-      .map(x => {
+export default function RandomColor() {
+  const [color, setColor] = useState('#000'); // update color
+  const getRgb = () => Math.floor(Math.random() * 256); // generate random numbers
+  const rgbToHex = (r, g, b) =>
+    '#' +
+    [r, g, b]
+      .map((x) => {
         const hex = x.toString(16);
         return hex.length === 1 ? '0' + hex : hex;
       })
-      .join ('');
+      .join('');
 
-    const generateColor = () => {
-      const color = {
-        r: getRgb(),
-        g: getRgb(),
-        b: getRgb(),
-      };
+  const generateColor = () => {
+    const color = {
+      r: getRgb(),
+      g: getRgb(),
+      b: getRgb(),
+    };
 
     setColor(rgbToHex(color.r, color.g, color.b));
-    };
+  };
 
   return (
     <div>
-      <button style= {{ color: color }} // create button
-          onClick ={generateColor}
-        >
-     <h1 style={{fontFamily:'Verdana'}}> Click</h1>
-     <h2 style={{fontFamily:'Verdana'}}>Don't be shy</h2>
-     </button>
+      <button
+        class="glow-on-hover"
+        style={{ color: 'white' }} // create button
+        onClick={generateColor}
+      >
+        <div className="click">
+          <p style={{ fontFamily: 'Verdana' }}> Click</p>
+          <p style={{ fontFamily: 'Verdana' }}>Don't be shy</p>
+        </div>
+      </button>
 
-     <div style={{ backgroundColor: color}} className='container'>
-        <a style={{ backgroundColor: color, fontFamily:'Verdana', verticalAlign: 'middle'}}>Color : {color}</a>
+      <div style={{ backgroundColor: color }} className="container">
+        <p>Generated Color : {color}</p>
       </div>
     </div>
-
-  )
-
+  );
 }
--- src/PropsGreeting.js
+++ src/PropsGreeting.js
@@ -1,13 +1,17 @@
+import './PropsGreeting.css';
+import './App.css';
+
 export default function PropsGreeting(props) {
   console.log('props', props); // Log out all props
   return (
     <div
+      className="greeting"
       style={{
         color: '#1D10C1',
-        fontSize: '50px',
-        width: '100%',
         fontweight: 'bold',
-        fontFamily:'Verdana'
+        fontFamily: 'Verdana',
+
+        //backgroundColor: '#FBE3B7',
       }}
     >
       Welcome to the Random Color Generator !
